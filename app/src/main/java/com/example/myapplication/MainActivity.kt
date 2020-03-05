237b14f27737373c159fedd7e4584b4db34d0e58
taruc <weiyangtan1999@gmail.com>
2020-03-05 12:34:00 +0800

    i so smart

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextClock
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,25 +14,66 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
     }
 
-    // for random number
-    /*
-    private fun rollDice(){
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = (1..6).random() //random number 1-6
-        resultText.text = randomInt.toString()
+    private fun rollDice() {
+       // val resultText: TextView = findViewById(R.id.result_text)
+       // val randomInt = (1..6).random() //random number 1-6
+       // resultText.text = randomInt.toString()
 
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
-    }*/
+//        Toast.makeText(this, "button clicked",
+//            Toast.LENGTH_SHORT).show()
 
-    private fun rollDice(){
-        val resultText: TextView = findViewById(R.id.result_text)
-        val num = 0
-        resultText.text = num.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
 
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+        diceImage.setImageResource(imagerand())
+        diceImage2.setImageResource(imagerand())
+
+//        val drawableResource = when (randomInt) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            else -> R.drawable.dice_6
+//        }
+
+        //diceImage.setImageResource(drawableResource)
     }
+
+    private fun imagerand(): Int
+    {
+        val randomInt = (1..6).random()
+
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+
+//    private fun countUp() {
+//        val resultText: TextView = findViewById(R.id.result_text)
+//
+//        //if text is the default "Hello Wolrd" set that to 1
+//        if (resultText.text == "Hello World") {
+//            resultText.text = "1"
+//        } else {
+//            // Othersive, increament the number up to 6.
+//            //The text value in resultText.text is an rostance of the CharSequence class;
+//            //it need to be converted to a String object before it can be converted to an int.
+//            var resultInt = resultText.text.toString().toInt()
+//
+//            if (resultInt < 6) {
+//                resultInt++
+//                resultText.text = resultInt.toString()
+//            }
+//        }
+//    }
+
 }
